807ce8db1582fd3b4fd97c882935ad57b2917ddd
Jonathan Guevara <memooguevara@gmail.com>
2024-10-17 16:27:43 -0500

    feat: update lsp keymaps

--- config/core/lsp.nix
+++ config/core/lsp.nix
@@ -1,17 +1,34 @@
 {
   plugins.lsp = {
     enable = true;
-    keymaps.lspBuf = {
-      K = "hover";
-      gD = "references";
-      gd = "definition";
-      gi = "implementation";
-      gt = "type_definition";
+    keymaps = {
+      diagnostic = {
+        "<leader>j" = "goto_next";
+        "<leader>k" = "goto_prev";
+      };
+      lspBuf = {
+        K = "hover";
+        gD = "references";
+        gd = "definition";
+        gi = "implementation";
+        gt = "type_definition";
+        "<leader>vrn" = "rename";
+      };
     };
     onAttach = ''
       vim.keymap.set('n', '<leader>vf', function()
         vim.lsp.buf.format({async = true})
       end)
+
+      vim.keymap.set('i', '<c-t>', function()
+        vim.lsp.buf.signature_help()
+      end)
+
+      vim.keymap.set({'v', 'n'}, '<leader>vca', vim.lsp.buf.code_action)
     '';
   };
+
+  extraConfigLua = ''
+    vim.lsp.inlay_hint.enable()
+  '';
 }
